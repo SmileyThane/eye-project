8fa5a3564f786e6f7b95c34cd5d065f5b8cc2f3c
Hleb Prakhnitski <thane.crios@gmail.com>
2021-06-19 11:39:59 +0300

    added StoredResourceController

---
+++ app/Http/Controllers/StoredResource/StoredResourceController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers\StoredResource;
+
+use App\Http\Controllers\Controller;
+
+class StoredResourceController extends Controller
+{
+    public function index()
+    {
+
+    }
+
+    public function find($id)
+    {
+
+    }
+
+    public function create()
+    {
+
+    }
+
+    public function store()
+    {
+
+    }
+
+    public function udpate($id)
+    {
+
+    }
+
+    public function edit($id)
+    {
+
+    }
+
+    public function delete($id)
+    {
+
+    }
+}
