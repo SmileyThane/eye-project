ae11c2ef9a18198b8045f7f700d9338c719cdce5
Hleb Prakhnitski <thane.crios@gmail.com>
2021-06-19 12:23:26 +0300

    added fillable props for stored_resources model

--- app/Models/StoredResource.php
+++ app/Models/StoredResource.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class StoredResource extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'id',
+        'user_id',
+        'name',
+        'protocol',
+        'domain',
+        'port',
+        'ip',
+        'is_active_ssl',
+        'ssl_expired_at',
+        'is_active',
+        'last_status',
+        'last_request_execution_time',
+        'last_checked_at',
+        'created_at',
+        'updated_at'
+    ];
 }
