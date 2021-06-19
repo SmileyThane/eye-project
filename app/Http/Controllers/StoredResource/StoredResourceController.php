8303106b0a9b1c3795c0bad72ffbb5dcb3c66ff0
Hleb Prakhnitski <thane.crios@gmail.com>
2021-06-19 12:23:43 +0300

    updated stored_resources controller

--- app/Http/Controllers/StoredResource/StoredResourceController.php
+++ app/Http/Controllers/StoredResource/StoredResourceController.php
@@ -3,17 +3,20 @@
 namespace App\Http\Controllers\StoredResource;
 
 use App\Http\Controllers\Controller;
+use App\Models\StoredResource;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class StoredResourceController extends Controller
 {
     public function index()
     {
-
+        $storedResources = StoredResource::query()->where(['user_id' => Auth::id()])->get();
     }
 
     public function find($id)
     {
-
+        $storedResource = StoredResource::query()->find($id);
     }
 
     public function create()
@@ -21,9 +24,9 @@ class StoredResourceController extends Controller
 
     }
 
-    public function store()
+    public function store(Request $request)
     {
-
+        StoredResource::query()->create($request->all());
     }
 
     public function udpate($id)
@@ -31,13 +34,13 @@ class StoredResourceController extends Controller
 
     }
 
-    public function edit($id)
+    public function edit(Request $request, $id)
     {
-
+        StoredResource::query()->where(['id' => $id])->update($request->all());
     }
 
     public function delete($id)
     {
-
+        StoredResource::query()->where(['id' => $id])->delete();
     }
 }
