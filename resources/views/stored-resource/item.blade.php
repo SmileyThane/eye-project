17059d3b93a9134fd6c6b1ae2d57e9ef2b3b3a21
Hleb Prakhnitski <thane.crios@gmail.com>
2021-06-20 21:55:21 +0300

    created Stored Resources single item page

---
+++ resources/views/stored-resource/item.blade.php
@@ -0,0 +1,83 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('Register') }}</div>
+
+                    <div class="card-body">
+                        <form method="POST" action="{{ route('store-stored-resources') }}">
+                            @csrf
+
+                            <div class="form-group row">
+                                <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+
+                                <div class="col-md-6">
+                                    <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+
+                                    @error('name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label for="protocol" class="col-md-4 col-form-label text-md-right">{{ __('Protocol') }}</label>
+
+                                <div class="col-md-6">
+                                    <input id="protocol" type="text" class="form-control @error('protocol') is-invalid @enderror" name="protocol" value="{{ old('protocol') }}" required autocomplete="protocol" autofocus>
+
+                                    @error('protocol')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label for="domain" class="col-md-4 col-form-label text-md-right">{{ __('domain') }}</label>
+
+                                <div class="col-md-6">
+                                    <input id="domain" type="text" class="form-control @error('domain') is-invalid @enderror" name="domain" value="{{ old('domain') }}" required autocomplete="domain" autofocus>
+
+                                    @error('domain')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label for="port" class="col-md-4 col-form-label text-md-right">{{ __('port') }}</label>
+
+                                <div class="col-md-6">
+                                    <input id="port" type="text" class="form-control @error('port') is-invalid @enderror" name="port" value="{{ old('port') }}"  autocomplete="port" autofocus>
+
+                                    @error('port')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="form-group row mb-0">
+                                <div class="col-md-6 offset-md-4">
+                                    <button type="submit" class="btn btn-primary">
+                                        {{ __('Register') }}
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
