7c83dd19636d133156c6aaefd426f7b52dc4edff
Hleb Prakhnitski <thane.crios@gmail.com>
2021-06-22 00:03:22 +0300

    updated item page for stored resources

--- resources/views/stored-resource/item.blade.php
+++ resources/views/stored-resource/item.blade.php
@@ -8,73 +8,97 @@
                     <div class="card-header">{{ __('Register') }}</div>
 
                     <div class="card-body">
-                        <form method="POST" action="{{ route('store-stored-resources') }}">
-                            @csrf
+                        @if(request()->id)
+                            <form method="POST" action="{{ route('edit-stored-resource', ['id' => request()->id])}}">
+                                @method('PATCH')
+                                @else
+                                    <form method="POST" action="{{route('store-stored-resources') }}">
+                                        @endif
+                                        @csrf
 
-                            <div class="form-group row">
-                                <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+                                        <div class="form-group row">
+                                            <label for="name"
+                                                   class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
 
-                                <div class="col-md-6">
-                                    <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                                            <div class="col-md-6">
+                                                <input id="name" type="text"
+                                                       class="form-control @error('name') is-invalid @enderror"
+                                                       name="name"
+                                                       value="{{  $storedResource['name'] ??  old('name') }}" required
+                                                       autocomplete="name" autofocus>
 
-                                    @error('name')
-                                    <span class="invalid-feedback" role="alert">
+                                                @error('name')
+                                                <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
-                                    @enderror
-                                </div>
-                            </div>
+                                                @enderror
+                                            </div>
+                                        </div>
 
-                            <div class="form-group row">
-                                <label for="protocol" class="col-md-4 col-form-label text-md-right">{{ __('Protocol') }}</label>
+                                        <div class="form-group row">
+                                            <label for="protocol"
+                                                   class="col-md-4 col-form-label text-md-right">{{ __('Protocol') }}</label>
 
-                                <div class="col-md-6">
-                                    <input id="protocol" type="text" class="form-control @error('protocol') is-invalid @enderror" name="protocol" value="{{ old('protocol') }}" required autocomplete="protocol" autofocus>
+                                            <div class="col-md-6">
+                                                <input id="protocol" type="text"
+                                                       class="form-control @error('protocol') is-invalid @enderror"
+                                                       name="protocol"
+                                                       value="{{ $storedResource['protocol'] ?? old('protocol') }}"
+                                                       required autocomplete="protocol" autofocus>
 
-                                    @error('protocol')
-                                    <span class="invalid-feedback" role="alert">
+                                                @error('protocol')
+                                                <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
-                                    @enderror
-                                </div>
-                            </div>
+                                                @enderror
+                                            </div>
+                                        </div>
 
-                            <div class="form-group row">
-                                <label for="domain" class="col-md-4 col-form-label text-md-right">{{ __('domain') }}</label>
+                                        <div class="form-group row">
+                                            <label for="domain"
+                                                   class="col-md-4 col-form-label text-md-right">{{ __('domain') }}</label>
 
-                                <div class="col-md-6">
-                                    <input id="domain" type="text" class="form-control @error('domain') is-invalid @enderror" name="domain" value="{{ old('domain') }}" required autocomplete="domain" autofocus>
+                                            <div class="col-md-6">
+                                                <input id="domain" type="text"
+                                                       class="form-control @error('domain') is-invalid @enderror"
+                                                       name="domain"
+                                                       value="{{ $storedResource['domain'] ?? old('domain') }}" required
+                                                       autocomplete="domain" autofocus>
 
-                                    @error('domain')
-                                    <span class="invalid-feedback" role="alert">
+                                                @error('domain')
+                                                <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
-                                    @enderror
-                                </div>
-                            </div>
+                                                @enderror
+                                            </div>
+                                        </div>
 
-                            <div class="form-group row">
-                                <label for="port" class="col-md-4 col-form-label text-md-right">{{ __('port') }}</label>
+                                        <div class="form-group row">
+                                            <label for="port"
+                                                   class="col-md-4 col-form-label text-md-right">{{ __('port') }}</label>
 
-                                <div class="col-md-6">
-                                    <input id="port" type="text" class="form-control @error('port') is-invalid @enderror" name="port" value="{{ old('port') }}"  autocomplete="port" autofocus>
+                                            <div class="col-md-6">
+                                                <input id="port" type="text"
+                                                       class="form-control @error('port') is-invalid @enderror"
+                                                       name="port" value="{{ $storedResource['port'] ?? old('port') }}"
+                                                       autocomplete="port" autofocus>
 
-                                    @error('port')
-                                    <span class="invalid-feedback" role="alert">
+                                                @error('port')
+                                                <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
-                                    @enderror
-                                </div>
-                            </div>
+                                                @enderror
+                                            </div>
+                                        </div>
 
-                            <div class="form-group row mb-0">
-                                <div class="col-md-6 offset-md-4">
-                                    <button type="submit" class="btn btn-primary">
-                                        {{ __('Register') }}
-                                    </button>
-                                </div>
-                            </div>
-                        </form>
+                                        <div class="form-group row mb-0">
+                                            <div class="col-md-6 offset-md-4">
+                                                <button type="submit" class="btn btn-primary">
+                                                    {{ __('Submit') }}
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </form>
                     </div>
                 </div>
             </div>
