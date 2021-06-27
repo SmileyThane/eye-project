87bf2d158e3d009a2055699b77e7fb3c02c4e77c
Hleb Prakhnitski <thane.crios@gmail.com>
2021-06-27 23:12:13 +0300

    added StoredResourceHistoryController

---
+++ app/Http/Controllers/StoredResource/StoredResourceHistoryController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\StoredResource;
+
+use App\Http\Controllers\Controller;
+use App\Models\StoredResourceStatusHistory;
+use Illuminate\Http\Request;
+
+class StoredResourceHistoryController extends Controller
+{
+    public function store($id, $contentId, $executionTime, $status): StoredResourceStatusHistory
+    {
+        $storedResourceStatusHistory = new StoredResourceStatusHistory();
+        $storedResourceStatusHistory->stored_resource_id = $id;
+        $storedResourceStatusHistory->stored_resource_content_id = $contentId;
+        $storedResourceStatusHistory->request_execution_time = $executionTime;
+        $storedResourceStatusHistory->status = $status;
+        $storedResourceStatusHistory->save();
+
+        return $storedResourceStatusHistory;
+    }
+}
