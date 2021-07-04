5a1fb04a6f650905e209ac98d661b1b2931e799b
Hleb Prakhnitski <thane.crios@gmail.com>
2021-07-04 23:46:09 +0300

    updated StoredResourceContentController

--- app/Http/Controllers/StoredResource/StoredResourceContentController.php
+++ app/Http/Controllers/StoredResource/StoredResourceContentController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\StoredResource;
 
 use App\Http\Controllers\Controller;
 use App\Models\StoredResourceContent;
-use Illuminate\Http\Request;
 
 class StoredResourceContentController extends Controller
 {
